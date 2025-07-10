207f0c21d2d61073b20a2bebbd48b52e8077aba1
codewith-Ansh <anshh.ce@gmail.com>
2025-07-10 21:52:41 +0530

    Variables and Datatypes in Java

---
+++ Extra/Datatypes.java
@@ -0,0 +1,35 @@
+public class Datatypes {
+    public static void main(String[] args){
+
+        // -------- Primitive --------
+        byte age = 127;
+        int phone = 999999999;
+        long num = 999999999999999999L;
+        float pi = 3.14F;
+        char letter = 'a';
+        boolean check = true;
+
+        System.out.println(age);
+        System.out.println(phone);
+        System.out.println(num);
+        System.out.println(pi);
+        System.out.println(check);
+        System.out.println(letter);
+
+
+        /* ******* DEFAULT VALUES GIVEN WHEN THERE'S NO INITIALIZATION *******
+
+            | Data Type                      | Default Value        |
+            | ------------------------------ | -------------------- |
+            | `int`, `byte`, `short`, `long` | `0`                  |
+            | `float`, `double`              | `0.0`                |
+            | `char`                         | `\u0000` (null char) |
+            | `boolean`                      | `false`              |
+            | Object references              | `null`               |
+            
+         */
+
+        // -------- Non-Primitive --------
+        // strings, arrays, class, objects, enum, wrapper class, interface
+    }
+}
