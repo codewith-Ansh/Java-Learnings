dcb658d597e6db9286c38610138376023f0d9255
codewith-Ansh <anshh.ce@gmail.com>
2025-07-11 23:19:51 +0530

    Logical Operators

---
+++ Extra/Logical_Operators.java
@@ -0,0 +1,21 @@
+public class Logical_Operators {
+    public static void main(String[] args) {
+        // Logical operators
+        // &&
+        int a = 30, b = 60;
+        if(a < 50 && b < 50)
+            System.out.println("Both are less than 50!");
+        else
+            System.out.println("May be one or both are bigger!");
+
+        // ||
+        if(a < 50 || b < 50)
+            System.out.println("Both are less than 50!");
+        else
+            System.out.println("May be one or both are bigger!");
+
+        // !
+        
+
+    }
+}
