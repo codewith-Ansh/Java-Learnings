c8e5247ca58809fb383c52b444a523ddd184dcdf
codewith-Ansh <anshh.ce@gmail.com>
2025-07-12 21:35:21 +0530

    Type casting in Java

---
+++ Extra/Casting.java
@@ -0,0 +1,16 @@
+public class Casting {
+    public static void main(String[] args){
+        // casting
+        // ----- IMPLICIT (smaller to bigger) -----
+        double price = 100.00;
+        double finalPrice = price + 18;
+        System.out.println(finalPrice); // 118.0
+
+        // ----- EXPLICIT (bigger to smaller) -----
+        int p = 100;
+        // int fP = p + 18.0;  // not allowed because 18.0 != 18
+
+        int fP = p + (int)18.99;    // values after . will be truncated
+        System.out.println(fP);     // 118
+    }
+}
