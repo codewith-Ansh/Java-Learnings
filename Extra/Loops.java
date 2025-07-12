a5b34c4d5e87a97ee76b1efe29262a2cbd880f9b
codewith-Ansh <anshh.ce@gmail.com>
2025-07-12 21:36:00 +0530

    for, while and do while loop in java

---
+++ Extra/Loops.java
@@ -0,0 +1,20 @@
+public class Loops {
+    public static void main(String[] args) {
+        for(int i=0;i<=100;i=i+2){
+            System.out.println(i);
+        }
+
+        int i = 0;
+        while (true) {
+            i--;
+            System.out.println(i);
+            if(i==-100)
+                break;
+        }
+        int num = 100;
+        do{
+            System.out.println(num);
+            num = num - 5;
+        }while(num!=5);
+    }
+}
