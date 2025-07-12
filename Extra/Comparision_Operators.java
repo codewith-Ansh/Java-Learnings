e411ff62823d17714c849994411bcdbafa2e3310
codewith-Ansh <anshh.ce@gmail.com>
2025-07-12 21:34:00 +0530

    Comparision operators in Java

---
+++ Extra/Comparision_Operators.java
@@ -0,0 +1,13 @@
+public class Comparision_Operators {
+    public static void main(String[] args) {
+        // comparision operators
+        /*
+         * a == b
+         * a != b
+         * a < b
+         * a > b
+         * a <= b
+         * a >= b
+         */
+    }
+}
