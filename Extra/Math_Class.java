343582dccb3881ed846530a79b5609a228d9ecd3
codewith-Ansh <anshh.ce@gmail.com>
2025-07-12 21:34:51 +0530

    Basic Math class functionalities

---
+++ Extra/Math_Class.java
@@ -0,0 +1,13 @@
+public class Math_Class {
+    public static void main(String[] args){
+        // Math class
+        int a = 100;
+        int b = 80;
+
+        System.out.println(Math.max(a, b)); // 100
+        System.out.println(Math.min(a, b)); // 80
+
+        System.out.println(Math.random());  // random value b/w 0 to 1
+        System.out.println((int)(Math.random()*10));  // integer random value b/w 0 to 10
+    }
+}
