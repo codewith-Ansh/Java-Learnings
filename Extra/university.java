1a241a65519e2193be3ad98e4c6bb4b489a50d0d
codewith-Ansh <anshh.ce@gmail.com>
2025-07-13 21:28:33 +0530

    additional class file

---
+++ Extra/university.java
@@ -0,0 +1,12 @@
+public class university {
+    String uniName = null;
+    String uniAddress = null;
+
+    public void setValues(String uniName, String uniAddress){
+        this.uniName = uniName;
+        this.uniAddress = uniAddress;
+    }
+    public void display(){
+        System.out.println(uniName + ", " + uniAddress);
+    }
+}
