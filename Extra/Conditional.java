827c2a09ba6d515cc7edc2ff37895a6bd70a11f1
codewith-Ansh <anshh.ce@gmail.com>
2025-07-13 21:26:48 +0530

    Conditional statements

---
+++ Extra/Conditional.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Conditional {
+    public static void main(String[] args) {
+        boolean isSunUp;
+        System.out.println("Is sun up?(y/n): ");
+
+        Scanner sc = new Scanner(System.in);
+        String check = sc.next();
+        if(check.equals("y") || check.equals("Y")){
+            isSunUp = true;
+        }
+        else{
+            isSunUp =  false;
+        }
+        if(isSunUp){
+            System.out.println("Day!");
+        }
+        else{
+            System.out.println("Night!");
+        }
+    }
+}
