7b6490818480bde1b62e5ce5bf12db25326a48ea
codewith-Ansh <anshh.ce@gmail.com>
2025-07-18 23:02:32 +0530

    this keyword and its uses

---
+++ Extra/This.java
@@ -0,0 +1,16 @@
+class This {
+    int x;
+
+    This(int x) {
+        this.x = x;  // 'this' refers to the current object
+    }
+
+    void display() {
+        System.out.println("Value of x: " + this.x);
+    }
+
+    public static void main(String[] args) {
+        This obj = new This(10);
+        obj.display();
+    }
+}
