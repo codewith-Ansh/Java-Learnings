a8c6c133ad12eeb58efbea26fe89559d5380d01e
codewith-Ansh <anshh.ce@gmail.com>
2025-07-19 22:22:09 +0530

    used concepts of inner class, method-local inner class, toString() and Override

--- Lab/Practical 1/student.java
+++ Lab/Practical 1/student.java
@@ -2,80 +2,100 @@ import java.util.Objects;
 
 public class student {
     String id, name, branch;
+    double cgpa;
 
-    student(String id, String name, String branch) {
+    // Constructor
+    student(String id, String name, String branch, double cgpa) {
         this.id = id;
         this.name = name;
         this.branch = branch;
+        this.cgpa = cgpa;
     }
 
-    public void addCourse(String courseName, int courseCredits) {
-        course newCourse = new course(courseName, courseCredits);
-        System.out.println("Added Course: " + newCourse.courseName + " with Credits: " + newCourse.courseCredits);
+    // Inner Class - Course
+    public class Course {
+        String courseName;
+        int courseCredits;
+
+        Course(String courseName, int courseCredits) {
+            this.courseName = courseName;
+            this.courseCredits = courseCredits;
+        }
+
+        void showCourse() {
+            System.out.println("Course: " + courseName + ", Credits: " + courseCredits);
+        }
     }
 
-    public void enrollSemester(String courseName, int courseCredits) {
-        course newCourse = new course(courseName, courseCredits);
-        System.out.println("Enrolled in Course: " + newCourse.courseName + " with Credits: " + newCourse.courseCredits);
+    // Method-local Inner Class - Semester-wise enrollment
+    public void enrollInSemester(int sem, String courseName, int credits) {
+        class Semester {
+            void enroll() {
+                System.out.println(name + " enrolled in Semester " + sem);
+                Course c = new Course(courseName, credits);
+                c.showCourse();
+            }
+        }
+
+        Semester semester = new Semester();
+        semester.enroll();
     }
 
+    // Simple scholarship check
     public void checkScholarshipEligibility() {
-        System.out.println("Checking scholarship eligibility for student: " + name);
-        // Logic to check scholarship eligibility can be added here
+        System.out.println("Checking scholarship eligibility for " + name);
+        if (cgpa >= 8.0) {
+            System.out.println("Eligible for Scholarship");
+        } else {
+            System.out.println("Not Eligible for Scholarship");
+        }
     }
 
-    public void displayDetails() {
-        System.out.println("Student ID: " + id);
-        System.out.println("Student Name: " + name);
-        System.out.println("Student Branch: " + branch);
-        System.out.println("-------------------------");
-    }
-
+    // Override toString()
     public String toString() {
-        return ("ID: " + id + ", Name: " + name + ", Branch: " + branch);
+        return "ID: " + id + ", Name: " + name + ", Branch: " + branch + ", CGPA: " + cgpa;
     }
 
+    // Override equals()
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true; // Check if both references point to the same object
-        if (obj == null || getClass() != obj.getClass())
-            return false; // Check for null and class type
-        student other = (student) obj; // Cast to student type
-        return Objects.equals(id, other.id); // Compare based on id
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        student other = (student) obj;
+        return Objects.equals(id, other.id);
     }
 
+    // Override hashCode()
     @Override
     public int hashCode() {
         return Objects.hash(id);
     }
 
-    public class course {
-        String courseName;
-        int courseCredits;
-
-        course(String courseName, int courseCredits) {
-            this.courseName = courseName;
-            this.courseCredits = courseCredits;
-        }
-    }
-
+    // Main Method
     public static void main(String[] args) {
-        student s1 = new student("22", "Ansh Darji", "CE");
-        student s2 = new student("150", "Narendra Modi", "Civil");
-        student s3 = new student("07", "Mahendra singh Dhoni", "BCA");
-        student s4 = new student("22", "Ansh Darji", "CE");
+        student s1 = new student("22", "Ansh Darji", "CE", 8.5);
+        student s2 = new student("150", "Jay parmar", "Civil", 7.5);
+        student s3 = new student("07", "Vraj Baria", "BCA", 9.1);
+        student s4 = new student("22", "Ansh Darji", "CE", 8.5);
 
-        // System.out.println(s1);
-        s1.displayDetails();
+        // Display details
+        System.out.println(s1);
         System.out.println("s1 equals s2? " + s1.equals(s2));
         System.out.println("s1 equals s4? " + s1.equals(s4));
-        System.out.println("-------------------------");
 
-        // checking the hashcode
+        System.out.println("HashCodes:");
         System.out.println("s1.hashCode(): " + s1.hashCode());
         System.out.println("s2.hashCode(): " + s2.hashCode());
         System.out.println("s3.hashCode(): " + s3.hashCode());
         System.out.println("s4.hashCode(): " + s4.hashCode());
+
+        System.out.println("------------------ ENROLLMENT ------------------");
+        s1.enrollInSemester(3, "OOP", 4);
+        s2.enrollInSemester(3, "Mathematics", 3);
+
+        System.out.println("-------------- SCHOLARSHIP CHECK ---------------");
+        s1.checkScholarshipEligibility();
+        s2.checkScholarshipEligibility();
+        s3.checkScholarshipEligibility();
     }
 }
