46ee2a0ef9b3dd284bb616970ca4f86279dfa1e9
codewith-Ansh <anshh.ce@gmail.com>
2025-08-07 21:59:48 +0530

    Simple program for 3 digit armstrong number

---
+++ Extra/Armstrong.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+class Armstrong {
+    int number;
+
+    Armstrong(int number) {
+        this.number = number;
+    }
+
+    void checkArmstrong() {
+        int original = number;
+        int sum = 0;
+
+        while (number != 0) {
+            int digit = number % 10;
+            sum += digit * digit * digit;
+            number /= 10;
+        }
+
+        if (original == sum) {
+            System.out.println(original + " is an Armstrong number.");
+        } else {
+            System.out.println(original + " is not an Armstrong number.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a 3-digit number: ");
+        int input = scanner.nextInt();
+
+        Armstrong obj = new Armstrong(input);
+        obj.checkArmstrong();
+
+        scanner.close();
+    }
+}
