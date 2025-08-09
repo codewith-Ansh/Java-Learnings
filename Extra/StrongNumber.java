581d2351e6d8c808e706786fd7907593bccd86a1
codewith-Ansh <anshh.ce@gmail.com>
2025-08-09 19:54:10 +0530

    Strong number program

---
+++ Extra/StrongNumber.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class StrongNumber {
+    public static boolean Strong(int n){
+        int temp = n;
+        int sum = 0;
+        
+        int digit;
+        while (temp != 0) {
+            digit = temp%10;
+            int mul = 1;
+            for(int i=1; i<=digit; i++){
+                mul *= i;
+            }
+            sum += mul;
+
+            temp /= 10;
+        }
+
+        return sum == n;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int n = sc.nextInt();
+        
+        if(Strong(n)){
+            System.out.println(n + " is a Strong number");
+        }
+        else{
+            System.out.println(n + " is not a Strong number");
+        }
+    }
+}
