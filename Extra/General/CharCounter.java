529540a06f4b8c90e1e1626cf5706ea442b5a56d
codewith-Ansh <anshh.ce@gmail.com>
2025-08-16 22:02:18 +0530

    CharCounter program

---
+++ Extra/General/CharCounter.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class CharCounter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        int letters = 0, digits = 0, spaces = 0, special = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+
+            if (Character.isLetter(ch)) {
+                letters++;
+            } else if (Character.isDigit(ch)) {
+                digits++;
+            } else if (Character.isWhitespace(ch)) {
+                spaces++;
+            } else {
+                special++;
+            }
+        }
+
+        System.out.println("Letters: " + letters);
+        System.out.println("Digits: " + digits);
+        System.out.println("Spaces: " + spaces);
+        System.out.println("Special Characters: " + special);
+
+        sc.close();
+    }
+}
